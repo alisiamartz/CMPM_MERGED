968e617a3a1004be67b50a9adef7f9581f001107
sonhuynh95 <sohuynh95@gmail.com>
2015-03-11 02:16:34 -0700

    Major mistake, replaced entire js by accident

--- PD_TEST_overworld/hackWindow.js
+++ PD_TEST_overworld/hackWindow.js
@@ -1,76 +1,323 @@
-/*
- *	inputs should automatically be taken in when hackBool == true;
- *	the sole variable that is keeping storing the input string is "inputs", located directly underneath
+/**
+ * @author Alisia Martinez
  */
-var input = "";
 
-/* object for keys and their matching keycode */
-var keys = {
-	backspace: [8],
-	enter: [13],
-	A:[65], B:[66], C:[67], D:[68], E:[69], F:[70], G:[71], H:[72], I:[73], J:[74], K:[75], L:[76], M:[77], 
-	N:[78], O:[79], P:[80], Q:[81], R:[82], S:[83], T:[84], U:[85], V:[86], W:[87], X:[88], Y:[89], Z:[90]
-};
+// THIS IS WHERE THE NEW HACKING STUFF WILL BE IMPLEMENTED
 
-/* Checks for any key input in addEventListener */
-Object.prototype.getKey = function(value) {
-	for (var key in this) {
-		if (this[key] instanceof Array && this[key].indexOf(value) >= 0) {
-			return key;
-		}
-	}
-	return null;
-};
+/*
+ * Variables for initiation
+ */
+var canvas;
+var h;
 
-/* checks for key input */
-addEventListener("keydown", function(e) {
-	lastKey = keys.getKey(e.keyCode);
-	if (hackBool == true) {
-		if (['enter'].indexOf(lastKey) >= 0) {
-			if (input.localeCompare("ATCKUP") == 0 || input.localeCompare("ATTACKUP") == 0) {
-				
-				playerStats.atckUp();
-				//condition to increase enemy stat depending on player stat
-			} else if (input.localeCompare("ATCK") == 0 || input.localeCompare("ATTACK") == 0) {
-				
-				playerStats.enemyLwrDef();
-				//condition to increase enemy stat depending on player stat
-			}  else if (input.localeCompare("ATCKDOWN") == 0 || input.localeCompare("ATTACKDOWN") == 0 || 
-				input.localeCompare("ATCKDWN") == 0 || input.localeCompare("ATTACKDWN") == 0 || 
-				input.localeCompare("ATCKDN") == 0 || input.localeCompare("ATTACKDN") == 0) {
-				
-				playerStats.enemyLwrAtck();
-				//condition to increase enemy stat depending on player stat
-			} else if (input.localeCompare("DEFUP") == 0 || input.localeCompare("DEFENSEUP") == 0) {
-			
-				playerStats.defUp();
-				//condition to increase enemy stat depending on player stat
-			} else if (input.localeCompare("MASKUP") == 0 || input.localeCompare("MSKUP") == 0 || 
-				input.localeCompare("MSK") == 0 || input.localeCompare("MASK") == 0) {
-				
-				playerStats.maskUp();
-				//condition to increase enemy stat depending on player stat
-			} else if (input.localeCompare("SECDOWN") == 0 || input.localeCompare("SCDOWN") == 0 || 
-				input.localeCompare("SECDWN") == 0 || input.localeCompare("SCDWN") == 0 || 
-				input.localeCompare("SECDN") == 0 || input.localeCompare("SCDN") == 0) {
-				
-				playerStats.enemyLwrSec();
-				//condition to increase enemy stat depending on player stat
-			}
-			enemyActions[Math.floor(Math.random() * enemyActions.length)].call(enemyStats);
-			input = "";
-		} else if (['backspace'].indexOf(lastKey) >= 0) {
-			if (input != "") {
-				input = input.substring(0, input.length - 1);
-			}
-		} else if (['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'].indexOf(lastKey) >= 0
-				&& input.length <= 12) {
-			input += lastKey;
-		}
+/*
+ * DISPLAYS HACK INTERFACE 
+ * (initiated when player enters correct code)
+ */
+
+
+function hackInter() {
+	canvas = document.getElementById("hackCanvas");
+	h = canvas.getContext("2d");
+	
+	// APPEARANCE TO BE RE FORMATTED LATER
+	h.font = "50px courier";
+	h.fillText("HACK STATS", 165,50,250); // Title something else, even just a cute gov seal thing could work to replace
+	h.fillText("YOU",100,100,75);
+	h.fillText("TARGET",350,100,150);
+	
+	// Player Stat Display (atack, deff, mask)
+	h.font = "30px courier";
+	h.fillText("ATCK "+ playerStats.atck, 100, 140, 200);
+	h.fillText("DEF "+ playerStats.def, 100, 180, 200);
+	h.fillText("MASK "+ playerStats.mask, 100,220,200);
+	
+	// Target Stat Display (atack, deff, mask)
+	h.fillText("ATCK "+ enemyStats.atck, 350, 140, 200);
+	h.fillText("DEF "+ enemyStats.def, 350, 180, 200);
+	h.fillText("MASK "+ enemyStats.sec, 350, 220, 200);
+		
+	// Starts input
+	h.fillText("Input: ", 5, 300, 100);
+	h.fillText(input, 100, 300, 400);
+}
+
+/*
+ * Display when player does not enter correct case Code
+ */
+function incorrectDisplay() {
+	canvas = document.getElementById("hackCanvas");
+	h = canvas.getContext("2d");
+	h.font = "70px courier";
+	h.fillText("INCORRECT CODE. PLEASE SEARCH DATABASE", 20,190,550);
+}
+
+/*
+ * Function to clear canvas when back button is pressed to prevent overlay
+ */
+function clearCanvas() {
+	canvas = document.getElementById("hackCanvas");
+	h = canvas.getContext("2d");
+	h.clearRect(0,0,590,390);
+}
+
+/*
+ * HACKING MECHANIC
+ */
+
+// CONSTANTS
+var INC_RATIO = 1.15;
+var MIN_INC = 10;
+var DEDUCT_VAR = 0.05;
+var MIN_STATS = 1;
+
+// CHANGING VARIABLES
+var string = "";
+var DIFF = 30;
+var hackBool = false;
+
+// THIS IS WHERE THE NEW HACKING STUFF WILL BE IMPLEMENTED
+
+/*************************************************************/
+/* this function starts the hacking mechanic	 			 */
+/*************************************************************/
+Hack = {
+	init: function(difficulty) {
+		playerStats.init(genPlayer(playerStats.staticA, playerStats.staticD, playerStats.staticM));
+		enemyStats.init(genEnemy(difficulty));
+		
+		/*
+		 *	initialize the display used to inform the player of what they are typing
+		 */
+	},
+	end: function() {
+		string = "";
+		hackBool = false;
 		clearCanvas();
-		hackInter();
 	}
-	/*
-	 *	update the display for what is being typed to nothing
-	 */
-}, false);
+};
+winLose = {
+	result: function(winBool) {
+		/*
+		 *	initialize the display used to inform the player of a success or failure
+		 */
+	},
+	end: function() {
+		/*
+		 *	remove the display used to inform the player of a success or failure
+		 */
+	}
+};
+var genPlayer = function(atck, def, mask) {
+	var genArr = [];
+	genArr.push(atck);
+	genArr.push(def);
+	genArr.push(mask);
+	return new crackStats(genArr[0], genArr[1], genArr[2]);
+};
+var genEnemy = function(difficulty) {
+	var genArr = [];
+	for (var i = 0; i < 3; i++) {
+		genArr.push(Math.floor(difficulty + (Math.random() * difficulty / 3)));
+	}
+	if (genArr[2] > 80) genArr[2] = 80;
+	return new crackStats(genArr[0], genArr[1], genArr[2]);
+};
+var crackStats = function(atck, def, msk) {
+	this.attack = atck;
+	this.defense = def;
+	this.mask = msk;
+};
+/*************************************************************/
+/* this function stores player stats and commands 			 */
+/*************************************************************/
+playerStats = {
+	atck: 50,
+	def: 100,
+	mask: 100,
+	staticA: 50,
+	staticD: 100,
+	staticM: 100,
+	init: function(stats) {
+		this.atck = stats.attack;
+		this.def = stats.defense;
+		this.mask = stats.mask;
+		
+		/*
+		 *	initialize the display used for stats here
+		 */
+	},
+	statUp: function(theStat) {
+		if (this[theStat] >= 70) this[theStat] = Math.floor(this[theStat] * (INC_RATIO + (Math.random() / 5)));
+		else this[theStat] += MIN_INC;
+	},
+	atckUp: function() {
+		this.statUp('atck');
+		/*
+		 *	update the display used for attack here
+		 */
+	},
+	defUp: function() {
+		this.statUp('def');
+		/*
+		 *	update the display used for defence here
+		 */
+	},
+	maskUp: function() {
+		this.statUp('mask');
+		/*
+		 *	update the display used for mask here
+		 */
+	},
+	enemyLwrAtck: function() {
+		enemyStats.selfLwrAtck(20);
+	},
+	enemyLwrDef: function() {
+		enemyStats.selfLwrDef(this.atck);
+	},
+	enemyLwrSec: function() {
+		enemyStats.selfLwrSec(10);
+	},
+	statDown: function(theStat, deduct) {
+		if (theStat == 'def') this[theStat] -= deduct;
+		else this[theStat] -= deduct + Math.floor(Math.random() * deduct * DEDUCT_VAR);
+		if (this[theStat] < MIN_STATS) {
+			if (theStat == 'def' || theStat == 'mask') Hack.end();
+			else this[theStat] = MIN_STATS;
+			winLose.result(false);
+		}
+	},
+	selfLwrAtck: function(deduct) {
+		this.statDown('atck', deduct);
+		/*
+		 *	update the display used for attack here
+		 */
+	},
+	selfLwrDef: function(deduct) {
+		this.statDown('def', deduct);
+		/*
+		 *	update the display used for defence here
+		 */
+	},
+	selfLwrMsk: function(deduct) {
+		this.statDown('mask', deduct);
+		/*
+		 *	update the display used for mask here
+		 */
+	},
+	end: function() {
+		/*
+		 *	clear the displays used for stats here, if they are instances then remove them
+		 */
+	}
+};
+/*************************************************************/
+/* this function stores enemy stats and commands 			 */
+/*************************************************************/
+enemyStats = {
+	atck: 5,
+	def: 10,
+	sec: 10,
+	init: function(stats) {
+		this.atck = stats.attack;
+		this.def = stats.defense;
+		this.sec = stats.mask;
+		
+		/*
+		 *	initialize the display used for stats here
+		 */
+	},
+	statUp: function(theStat) {
+		if (this[theStat] >= 70) this[theStat] = Math.floor(this[theStat] * (INC_RATIO + (Math.random() / 5)));
+		else this[theStat] += MIN_INC;
+		if (theStat == 'sec') {
+			if (this[theStat] >= 100) Hack.end();
+		}
+	},
+	atckUp: function() {
+		this.statUp('atck');
+		/*
+		 *	initialize the display used for attack here
+		 */
+	},
+	defUp: function() {
+		this.statUp('def');
+		/*
+		 *	initialize the display used for defence here
+		 */
+	},
+	secUp: function() {
+		this.statUp('sec');
+		/*
+		 *	initialize the display used for sec(enemy equivalent for mask in some aspect) here
+		 */
+	},
+	enemyLwrAtck: function() {
+		playerStats.selfLwrAtck(20);
+	},
+	enemyLwrDef: function() {
+		playerStats.selfLwrDef(this.atck);
+	},
+	enemyLwrMsk: function() {
+		playerStats.selfLwrMsk(10);
+	},
+	statDown: function(theStat, deduct) {
+		if (theStat == 'def') this[theStat] -= deduct;
+		else this[theStat] -= deduct + Math.floor(Math.random() * deduct * DEDUCT_VAR);
+		if (this[theStat] < MIN_STATS) {
+			if (theStat == 'def') Hack.end();
+			else this[theStat] = MIN_STATS;
+			winLose.result(true);
+		}
+	},
+	intelligence: function() {
+		if (this.atck > playerStats.def) {
+			this.enemyLwrDef();
+		} else if (this.atck < playerStats.atck) {
+			this.enemyLwrAtck();
+		} else if (this.sec < playerStats.mask) {
+			this.enemyLwrMsk();
+		} else if (this.def < playerStats.atck) {
+			this.defUp();
+		} else if (this.def > playerStats.def) {
+			this.atckUp();
+		} else {
+			this.secUp();
+		}
+	},
+	selfLwrAtck: function(deduct) {
+		this.statDown('atck', deduct);
+		/*
+		 *	update the display used for attack here
+		 */
+	},
+	selfLwrDef: function(deduct) {
+		this.statDown('def', deduct);
+		/*
+		 *	update the display used for defence here
+		 */
+	},
+	selfLwrSec: function(deduct) {
+		this.statDown('sec', deduct);
+		/*
+		 *	update the display used for sec(enemy equivalent for mask in some aspect) here
+		 */
+	},
+	end: function() {
+		/*
+		 *	clear the displays used for stats here, if they are instances then remove them
+		 */
+	}
+};
+
+var enemyActions = [
+	enemyStats['atckUp'],
+	enemyStats['defUp'],
+	enemyStats['secUp'],
+	enemyStats['enemyLwrAtck'],
+	enemyStats['enemyLwrDef'],
+	enemyStats['enemyLwrMsk']
+];
+for (var i = 0; i < 5; i++) {
+	enemyActions.push(enemyStats['intelligence']);
+}
