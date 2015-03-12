21221d1e35914925456a5b198ba7104062ead170
sonhuynh95 <sohuynh95@gmail.com>
2015-03-12 07:32:41 -0700

    Perfect length for input

--- PD_TEST_overworld/keyboard_input.js
+++ PD_TEST_overworld/keyboard_input.js
@@ -135,7 +135,7 @@ addEventListener("keydown", function(e) {
 				input = input.substring(0, input.length - 1);
 			}
 		} else if (['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'].indexOf(lastKey) >= 0
-				&& input.length <= 12) {
+				&& input.length <= 21) {
 			input += lastKey;
 		}
 		if (hackBool == true) {
