ab659e21cd2d755b0fffc38481c5ae9659281cf1
GreenCyan <oysong@ucsc.edu>
2015-03-11 23:56:40 -0700

    Further expansion
    sorry I didn't do more. I got a short fever...

--- PD_TEST_overworld/extraHacks.js
+++ PD_TEST_overworld/extraHacks.js
@@ -11,8 +11,41 @@ var hackMission = function () {
 	
 };
 
+
+var webcam = function () {
+  
+};
+
+var getDirt = function () {
+	
+	var outTask = new hackMission();
+	return outTask;
+	
+};
+
+// blackmail: set up an untracable connection, then send a blackmail message through it.
 var blackmail = function () {
 	
 	var outTask = new hackMission();
+	var blackSetup = outTask.playerSpecials;
+	
+	blackSetup['SETUP'] = function () {
+		if (Math.random() < 0.2) {
+			outTask.SpecialVars['SetUp'] = true;
+		}
+	};
+	
+	// Victory if 'SetUp' is true, otherwise instant fail
+	blackSetup['SEND'] = function () {
+		if (outTask.SpecialVars['SetUp'] = true) {
+			
+		} else {
+			
+		}
+	};
+	
+	outTask.SpecialVars['SetUp'] = false;
+	
+	return outTask;
   
-}
+};
